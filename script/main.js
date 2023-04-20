fa6abb7807610ad585504cb7cbc29dfa0707d54c
Lukas Sander <lukas.sander@vicotec.de>
2023-04-20 12:32:56 +0200

    listcontroller

--- script/main.js
+++ script/main.js
@@ -3,13 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let listTemplate = document.querySelector('#template_list');
     let listContainer = document.querySelector('.lists-container');
 
+    const listController = new ListController();
 
-    
+
+
 
     document.querySelector('#addList').addEventListener('click', () => {
         const clone = listTemplate.content.cloneNode(true);
         listContainer.append(clone);
 
-        initializeList(listContainer.querySelector('.table-list:last-child'));
+        listController.initializeList(listContainer.querySelector('.table-list:last-child'));
     })
 });
